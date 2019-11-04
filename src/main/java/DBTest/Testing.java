27d5985f89c6cb393a881a2fc2afda108d4813da
maestro2506 <miras.b2506@gmail.com>
2019-11-04 20:50:29 +0600

    Initial commit

---
+++ src/main/java/DBTest/Testing.java
@@ -0,0 +1,11 @@
+package DBTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Testing {
+    public static void main (String[] args) {
+        SpringApplication.run(Testing.class, args);
+    }
+}
